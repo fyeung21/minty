bad220a772e2168870e5bcd6a46b91a76dfcc338
Fiona <fyeung21@gmail.com>
2020-09-18 21:07:40 -0700

    feat: Make button generate new name

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 
 const App = () => {
 
-  // const [firstName, setFirstName] = useState('');
-  // const [secondName, setSecondName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
 
   const adjectives = ["pinky", "yellow", "lemony", "safe", "real", "praying"];
   const nouns = ["cat", "goat", "whale", "pen", "spoon", "shrimp"];
 
-  const randomAdjectives = adjectives.sort((a, b) => 0.5 - Math.random());
-  const randomNouns = nouns.sort((a, b) => 0.5 - Math.random());
-
-  const newAdjective = randomAdjectives[0];
-  const newNoun = randomNouns[0];
-
 
   const generateName = () => {
+    const randomAdjectives = adjectives.sort((a, b) => 0.5 - Math.random());
+    const randomNouns = nouns.sort((a, b) => 0.5 - Math.random());
 
+    const newAdjective = randomAdjectives[0];
+    const newNoun = randomNouns[0];
 
+    setFirstName(newAdjective);
+    setSecondName(newNoun);
   };
 
   return (
     <div className="App">
       <h1>minty fresh names</h1>
 
-      <p>{newAdjective + newNoun}</p>
+      <p>{firstName + secondName}</p>
 
       <button onClick={generateName}>
         Generate a new name!
