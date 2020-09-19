252ecde76d04e195ce3f22221f817aa203b4177c
Fiona <fyeung21@gmail.com>
2020-09-18 20:34:47 -0700

    feat: Randomize adjectives and nouns array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  // const [firstName, setFirstName] = useState('');
+  // const [secondName, setSecondName] = useState('');
+
+  const adjectives = ["pinky", "yellow", "lemony", "safe", "real", "praying"];
+  const nouns = ["cat", "goat", "whale", "pen", "spoon", "shrimp"];
+
+  const randomAdjectives = adjectives.sort((a, b) => 0.5 - Math.random());
+  const randomNouns = nouns.sort((a, b) => 0.5 - Math.random());
+
+  const newAdjective = randomAdjectives[0];
+  const newNoun = randomNouns[0];
+
+
+  const generateName = () => {
+
+
+  };
+
   return (
     <div className="App">
       <h1>minty fresh names</h1>
+
+      <p>{newAdjective + newNoun}</p>
+
+      <button onClick={generateName}>
+        Generate a new name!
+        </button>
+
     </div>
   );
 }
