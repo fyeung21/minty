d7d8cbcb691c39487937d6e5db90b00b04a1c18f
Fiona <fyeung21@gmail.com>
2020-09-26 21:08:38 -0700

    Updates

---
+++ precache-manifest.c6af244eefd45deca946c1f8187db91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d73374ffc516c018dbc3f96b0c2f45",
+    "url": "/minty/index.html"
+  },
+  {
+    "revision": "3d26d92472265799add4",
+    "url": "/minty/static/css/main.42fd75fd.chunk.css"
+  },
+  {
+    "revision": "058e9870b066cec8ab1c",
+    "url": "/minty/static/js/2.8229c878.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minty/static/js/2.8229c878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d26d92472265799add4",
+    "url": "/minty/static/js/main.3d0ae3f5.chunk.js"
+  },
+  {
+    "revision": "f12276fe96ff744ce951",
+    "url": "/minty/static/js/runtime-main.5327b481.js"
+  }
+]);
